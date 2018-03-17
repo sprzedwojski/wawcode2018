19ae2415725d41c8e275e0e2a2d80ce575377ef4
Szymon Przedwojski <szymon.przedwojski@bms.com.pl>
2018-03-17 11:06:16 +0100

    Dodanie material-ui (Material Design)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
+import { AppBar } from 'material-ui'
 import client from './client'
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
@@ -19,13 +19,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <AppBar title="Wawcode 2018"/>
                 <div>
                     {this.state.message}
                 </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { MuiThemeProvider } from 'material-ui/styles/index'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    (
+        <MuiThemeProvider>
+            <App/>
+        </MuiThemeProvider>
+    ), document.getElementById('root')
+)
 registerServiceWorker()
