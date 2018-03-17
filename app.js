eb8ccac4ef0d33c6d2fc69fd3556898bcdd5d5d5
Szymon Przedwojski <szymon.przedwojski@bms.com.pl>
2018-03-17 12:15:50 +0100

    Zakomentowanie połączenia z Mongo

--- app.js
+++ app.js
@@ -35,13 +35,13 @@ app.use(cors())
 /**
  * Connect to MongoDB.
  */
-mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI)
-mongoose.connection.on('error', (err) => {
-    console.error(err)
-    console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'))
-    process.exit()
-})
+// mongoose.Promise = global.Promise
+// mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI)
+// mongoose.connection.on('error', (err) => {
+//     console.error(err)
+//     console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'))
+//     process.exit()
+// })
 
 /**
  * Express configuration.
